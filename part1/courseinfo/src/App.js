26fc5813b754e844f9fd54904868c487681271ef
Favour Ifeanacho <favour.franki3@gmail.com>
2022-05-06 23:45:55 +0100

    Commit for part 1.1 of exercise 1.1.-1.2.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,30 @@
+const Header = (props) => {
+  return(
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+}
+
+//Header component
+const Content = (props) => {
+  return(
+    <>
+      <p>Title: {props.part} | number of exercise: {props.exercise}</p>
+    </>
+  );
+}
+
+//Total component
+const Total = (props) => {
+  return (
+    <>
+      <p>Total number of exercises: {props.firstExercise + props.secExercise + props.thirdExercise}</p>
+    </>
+  );
+}
+
+//App component 
 function App() {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +36,11 @@ function App() {
 
   return (
     <>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content part = {part1} exercise = {exercises1}  />
+      <Content part = {part2} exercise = {exercises2}  />
+      <Content part = {part3} exercise = {exercises3}  />
+      <Total firstExercise = {exercises1} secExercise = {exercises2} thirdExercise = {exercises3} />
     </>
   );
 }
