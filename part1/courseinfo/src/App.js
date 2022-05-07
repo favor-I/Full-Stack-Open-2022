f838d1776864f8eaaecc2a9a366cfc21274f8ad4
Favour Ifeanacho <favour.franki3@gmail.com>
2022-05-07 12:55:56 +0100

    Commit for part 1.5 of exercise 1.3.-1.5.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,21 @@
-//Variables declaration
-const course = 'Half Stack application development'
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name:'State of a component',
-    exercises: 14
-  }
-]
+//Variables declaration 
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name:'State of a component',
+      exercises: 14
+    }
+  ]
+}
 
 const Header = (props) => {
   return(
@@ -34,9 +36,9 @@ const Part = (props) => {
 const Content = () => {
   return(
     <>
-      <Part part = {parts[0].name} exercise = {parts[0].exercises}  />
-      <Part part = {parts[1].name} exercise = {parts[1].exercises}  />
-      <Part part = {parts[2].name} exercise = {parts[2].exercises}  />
+      <Part part = {course.parts[0].name} exercise = {course.parts[0].exercises}  />
+      <Part part = {course.parts[1].name} exercise = {course.parts[1].exercises}  />
+      <Part part = {course.parts[2].name} exercise = {course.parts[2].exercises}  />
     </>
   );
 }
@@ -54,9 +56,9 @@ const Total = (props) => {
 function App() {
   return (
     <>
-      <Header course = {course} />
+      <Header course = {course.name} />
       <Content/>
-      <Total firstExercise = {parts[0].exercises} secExercise = {parts[1].exercises} thirdExercise = {parts[2].exercises} />
+      <Total firstExercise = {course.parts[0].exercises} secExercise = {course.parts[1].exercises} thirdExercise = {course.parts[2].exercises} />
     </>
   );
 }
