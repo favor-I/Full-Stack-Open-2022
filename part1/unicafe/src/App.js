333569e553e93e5b6776022b0ec4e465768733ed
Favour Ifeanacho <favour.franki3@gmail.com>
2022-05-13 11:56:33 +0100

    Commit for part 1.6 of exercise 1.6.-1.1.4

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Button = ({funcToCall, text}) =>  <button onClick={funcToCall}>{text}</button>
+
+function App() {
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  // const handleGoodClicks = () => {
+  //   setClicks({...clicks, good: clicks.good + 1})
+  // }
+  // const handleNeutralClicks = () => {
+  //   setClicks({...clicks, neutral: clicks.neutral + 1})
+  // }
+  // const handleBadClicks = () => {
+  //   setClicks({...clicks, bad: clicks.bad + 1})
+  // }
+
+  return (
+    <>
+      <section>
+        <h2>Give Feedback</h2>
+        <Button funcToCall={ () => setClicks({...clicks, good: clicks.good + 1})} text = "Good" />
+        <Button funcToCall={() => setClicks({...clicks, neutral: clicks.neutral + 1})} text = "Neutral" />
+        <Button funcToCall={() => setClicks({...clicks, bad: clicks.bad + 1})} text = "Bad" />
+        <h2>Statistics</h2>
+        <p>Good: {clicks.good}</p>
+        <p>Neutral: {clicks.neutral}</p>
+        <p>Bad: {clicks.bad}</p>
+      </section>
+    </>
+  );
+}
+
+export default App;
